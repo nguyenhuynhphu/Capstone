e6b244a3ef455af87a553b7ae295742ca2a50bd5
huulinh99 <43189610+huulinh99@users.noreply.github.com>
2021-02-16 09:27:18 +0700

    Linh commit

---
+++ src/services/customer.tsx
@@ -0,0 +1,30 @@
+import request from '@/utils/request';
+
+export async function fetchLibarians({filterName, pagination}: any): Promise<any> {
+  return request(`/api/Staff?Name=${filterName}&PageNumber=${pagination}`);
+}
+export async function insertLibarian(values: any) {
+  return request('/api/Staff', {
+    method: 'POST',
+    body: JSON.stringify(values),
+  });
+}
+
+
+export async function editLibarian(values: any) {
+  return request(`/api/Staff?id=${values.id}`, {
+    method: 'PUT',
+    body: JSON.stringify(values),
+  });
+}
+
+export async function deleteLibarian(bookshelfs: any) {
+  var tmp = '';
+  bookshelfs.forEach((id: any) => {
+    tmp += `id=${id}&`;
+  });
+  console.log("TMP: " + tmp);
+  return request(`/api/Staff?` + tmp, {
+    method: 'DELETE',
+  });
+}
