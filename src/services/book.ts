4bc12a425df893feeabfdb1059d16d1f2b84ae53
nguyenhuynhphu <36849519+nguyenhuynhphu@users.noreply.github.com>
2021-04-12 21:17:01 +0700

    a

--- src/services/book.ts
+++
@@ -1,7 +0,0 @@
-//http://171.244.5.88:90/api/BookGroup
-import request from '@/utils/request';
-
-export async function fetchAllBook(bookGroupId: number, count: number): Promise<any> {
-  return request(`/api/Book?BookGroupId=${bookGroupId}&PageSize=1000`);
-}
-
