1a0bf5153eaca3ec4e40d514363b8f087af35787
nguyenhuynhphu <36849519+nguyenhuynhphu@users.noreply.github.com>
2021-05-03 13:59:24 +0700

    a

--- src/layouts/SecurityLayout.tsx
+++ src/layouts/SecurityLayout.tsx
@@ -56,6 +56,10 @@ class SecurityLayout extends React.Component<SecurityLayoutProps, SecurityLayout
       console.log("DONT HAVE PERMISSION !");
       return <Redirect to={`/no-permission`} />;
     }
+    if(url.indexOf('/borrow-book') != -1 && !isAdmin){
+      console.log("DONT HAVE PERMISSION !");
+      return <Redirect to={`/no-permission`} />;
+    }
     
     return children;
   }
